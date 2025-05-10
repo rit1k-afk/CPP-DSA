7d6149b931499e72d741cd1afb0c9d815327c838
Ritik Anand <152178879+rit1k-afk@users.noreply.github.com>
2025-05-10 21:07:14 +0530

    Added few more Data Structure Basic Codes

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,27 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void selectionSort(int arr[],int n){
-    for(int i=0; i< n-1; i++){
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
         int minIndex = i;
-        for(int j=i+1; j<n;j++){
-            if(arr[j]<arr[minIndex])
-            minIndex=j;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
         }
-        swap(arr[minIndex],arr[i]);
+        swap(arr[minIndex], arr[i]);
     }
 }
 
-void print(int arr[],int n){
-    for(int i = 0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }cout<<endl;
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
-int main(){
-    int arr[5]={33,43,12,27,8};
-    int arr1[5]={3,5,1,7,31};
-    selectionSort(arr,5);
-    selectionSort(arr1,5);
-    print(arr,5);
-    print(arr1,5);
+int main()
+{
+    int arr[5] = {33, 43, 12, 27, 8};
+    int arr1[5] = {3, 5, 1, 7, 31};
+    selectionSort(arr, 5);
+    selectionSort(arr1, 5);
+    print(arr, 5);
+    print(arr1, 5);
     return 0;
 }
